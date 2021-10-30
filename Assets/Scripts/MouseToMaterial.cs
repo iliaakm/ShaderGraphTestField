7ae118f28ff3d1c82b26f04181d43c1edb2e80cf
iliaakm <iliaakm@gmail.com>
2021-10-30 18:33:26 +0300

    Update MouseToMaterial.cs

--- Assets/Scripts/MouseToMaterial.cs
+++ Assets/Scripts/MouseToMaterial.cs
@@ -8,18 +8,14 @@ public class MouseToMaterial : MonoBehaviour
     private Material material;
     [SerializeField]
     private string mousePositionProperty;
-    [SerializeField]
-    Vector3 mousePosition;
-    [SerializeField]
-    Vector4 materialVector;
 
     // Update is called once per frame
     void Update()
     {
-        mousePosition = Input.mousePosition;
+        Vector3 mousePosition = Input.mousePosition;
         mousePosition.z = Camera.main.farClipPlane;
         Vector3 worldPoint = Camera.main.ScreenToWorldPoint(mousePosition);
-        materialVector = new Vector4(worldPoint.x, worldPoint.y, 0, 0);
+        Vector4 materialVector = new Vector4(worldPoint.x, worldPoint.y, 0, 0);
         material.SetVector(mousePositionProperty, materialVector);
     }
 }
