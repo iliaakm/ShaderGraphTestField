0673a5cfb3b62ea03049c4155970acff2f35640d
iliaakm <iliaakm@gmail.com>
2021-10-31 14:24:01 +0300

    Add Shader Showroom

---
+++ Assets/Scripts/ShaderSelector.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderSelector : MonoBehaviour
+{
+    [SerializeField]
+    private Material[] materials;
+    [SerializeField]
+    private Renderer targetRenderer;
+
+}
