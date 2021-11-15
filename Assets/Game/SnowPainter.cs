7ed5b077745db3ac2ecd533c10acd0f15d8aa39d
iliaakm <iliaakm@gmail.com>
2021-11-15 21:17:11 +0300

    Snow Painter

---
+++ Assets/Game/SnowPainter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowPainter : MonoBehaviour
+{
+    [SerializeField]
+    LineRenderer lineRenderer;
+    [SerializeField]
+    GameObject snowPainterVFX;
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            RaycastHit raycastHit;
+            Ray ray = new Ray(transform.transform.position, transform.forward);
+            if(Physics.Raycast(ray, out raycastHit))
+            {
+                lineRenderer.enabled = true;
+                lineRenderer.SetPosition(0, lineRenderer.transform.position);
+                lineRenderer.SetPosition(1, raycastHit.point);
+
+                snowPainterVFX.transform.position = raycastHit.point;
+            }
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            lineRenderer.enabled = false;
+        }
+    }
+}
